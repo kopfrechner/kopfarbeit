7202a3a538305accbbbcb9ba0c78517063682a82
Christoph Kopf <ckopf86@gmail.com>
2026-01-05 22:34:34 +0100

    feat: Add `showBreadcrumbs` configuration to conditionally render breadcrumbs, apply header margin, and refactor about page title.

--- src/config.ts
+++ src/config.ts
@@ -11,6 +11,7 @@ export const SITE = {
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
   showArchives: true,
   showBackButton: true, // show back button in post detail
+  showBreadcrumbs: false, // show breadcrumbs
   editPost: {
     enabled: false,
     text: "Edit page",
