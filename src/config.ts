dcc0f18415ace561addf872404bcf79605b611f6
Christoph Kopf <ckopf86@gmail.com>
2026-01-05 20:40:58 +0100

    feat: Update config description and language from German to English.

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ export const SITE = {
   website: "https://kopfarbeit.dev", // replace this with your deployed domain
   author: "Christoph Kopf",
   profile: "https://www.linkedin.com/in/christoph-kopf-9b7a74b2",
-  desc: "KopfarbeIT: Ein Deep Dive in das .NET-Ökosystem und modernes Software-Engineering. Begleite Christoph Kopf beim Erkunden neuer Technologien, teile wertvolle Coding-Insights und erlebe die Reise eines leidenschaftlichen Engineers.",
+  desc: "KopfarbeIT: A deep dive into the .NET ecosystem and modern software engineering. Join Christoph Kopf as he explores new technologies, shares coding insights, and documents the journey of a passionate engineer.",
   title: "KopfarbeIT",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
@@ -18,6 +18,6 @@ export const SITE = {
   },
   dynamicOgImage: true,
   dir: "ltr", // "rtl" | "auto"
-  lang: "de", // html lang code. Set this empty and default will be "en"
+  lang: "en", // html lang code. Set this empty and default will be "en"
   timezone: "Europe/Vienna", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 } as const;
