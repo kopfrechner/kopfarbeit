d6e272f13228a16f73c803eab40c243278b95772
Christoph Kopf <ckopf86@gmail.com>
2026-01-05 20:38:31 +0100

    feat: personalize site configuration and social links

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://kopfarbeit.dev", // replace this with your deployed domain
+  author: "Christoph Kopf",
+  profile: "https://www.linkedin.com/in/christoph-kopf-9b7a74b2",
+  desc: "KopfarbeIT: Ein Deep Dive in das .NET-Ökosystem und modernes Software-Engineering. Begleite Christoph Kopf beim Erkunden neuer Technologien, teile wertvolle Coding-Insights und erlebe die Reise eines leidenschaftlichen Engineers.",
+  title: "KopfarbeIT",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
@@ -12,12 +12,12 @@ export const SITE = {
   showArchives: true,
   showBackButton: true, // show back button in post detail
   editPost: {
-    enabled: true,
+    enabled: false,
     text: "Edit page",
     url: "https://github.com/satnaing/astro-paper/edit/main/",
   },
   dynamicOgImage: true,
   dir: "ltr", // "rtl" | "auto"
-  lang: "en", // html lang code. Set this empty and default will be "en"
-  timezone: "Asia/Bangkok", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  lang: "de", // html lang code. Set this empty and default will be "en"
+  timezone: "Europe/Vienna", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 } as const;
